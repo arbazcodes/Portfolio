d5e3b9959cb447cc88181d475fa914f03500d069
Arbaz Khan <arbazkhan5300@gmail.com>
2023-08-11 22:36:55 +0500

    modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,88 @@
-import React from 'react'
-import { Tilt } from 'react-tilt'
-import { motion } from 'framer-motion'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion, AnimatePresence } from 'framer-motion';
+import ai from "../assets/ai.png";
+import data from "../assets/data.png";
+import web from "../assets/web.png";
+import { useInView } from 'react-intersection-observer';
+
+
+
+const DomainCard = ({ index, title, icon }) => {
+    const [ref, inView] = useInView({
+    triggerOnce: true, // Ensures the animation only triggers once
+    threshold: 0.5, // Adjust this threshold as needed
+});
+    return (
+        <Tilt className="xs:w-[250px] w-64 items-center">
+        <AnimatePresence>
+            <motion.div
+                ref={ref}
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -100 }}
+                exit={{ opacity: 0, x: 100 }}
+                transition={{ type: "spring", duration: 0.75, delay: index * 0.5, ease: "easeOut"}}
+                className='w-full bg-gradient-to-l from-amber-100 via-pink-200 to-purple-600 p-[1px] rounded-[20px] shadow-car' style={{ zIndex: 1 }}>
+
+                <div options={{ max: 45, scale: 1, speed: 450 }} className=' min-h-[250px] rounded-[20px] px-12 py-9 bg-gradient-to-b from-gray-900 to-gray-800 justify-center text-center items-center flex-col'>
+                    <div className='flex flex-col items-center justify-center h-full'>
+                        <img src={icon} alt={title} className='w-auto h-16 object-contain items-center justify-center' />
+                        <h1 className='mt-5 font-highlight text-md font-bold'>{title}</h1>
+                    </div>
+                </div>
+            </motion.div>
+            </AnimatePresence>
+        </Tilt>
+    )
+}
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+   
+    const domain = [
+        {
+            title: "AI Eniginner",
+            icon: ai
+        },
+        {
+            title: "Data Analyst",
+            icon: data
+        },
+        {
+            title: "Web Developer",
+            icon: web
+        }
+    ]
+    return (
+        <>
+        <div className='h-full w-full text-white bg-gradient-to-b from-black to-slate-900 py-28 text-center justify-center items-center'>
+        <motion.div initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -50 }}
+                    transition={{ type: "spring", duration: 1 }} 
+                    style={{ zIndex: 1 }}>
+                <h1 className='text-6xl'>
+                    About  
+                </h1>
+            </motion.div>
+            <motion.div initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -50 }}
+                        transition={{ type: "spring", duration: 1}}
+                        style={{ zIndex: 1 }}>
+                <div className=' text-left w-auto text-xl m-28 mt-14 leading-8'>
+                    <p>Hey, and welcome to my corner of the digital world!</p><br/>
+                    <p>Ever since my childhood, I've been captivated by science and its endless possibilities. From crafting circuits to bringing Arduino Bluetooth cars and finger-tracking mice to life, my curiosity led me to GIK Institute, where I'm currently exploring the vast universe of knowledge. Coding is my heartbeat – from web development to diving deep into AI, RL, DL, and computer vision – I thrive on the blend of creativity and logic. But my interests don't stop at the virtual realm. With a strong grasp of system programming and hardware, I've ventured into the tangible world, merging the digital with the physical.</p><br/>
+                    <p>Beyond lines of code, data analysis and visualization hold a special place in my skill set. I find joy in unraveling insights from data and presenting them with tools like Data Studio, Tableau, and Excel. The essence of my journey lies in crafting projects that fuse technology with creativity, from brain-computer interfaces to turning components into functional wonders. Innovation, for me, is about nurturing ideas into reality, and that's where my passion truly thrives. Thanks for joining me on this adventure where every line of code, every circuit, and every insight is a step forward in our digital narrative.</p><br/>
+                </div>
+            </motion.div>
+            
+            <div className='flex flex-wrap gap-10 mt-20 text-center justify-center items-center'>
+                {domain.map((domain, index) => (<DomainCard key={domain.title} index = {index} {...domain}/>))}
+            </div>
+
+        </div>  
+        </>
+    )
 }
 
 export default About
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
 import logo from '../assets/logo.png';
 
 const NavBar = () => {
+
     const links = [
         {
             id: 1,
@@ -28,8 +29,29 @@ const NavBar = () => {
 
     const [nav, setNav] = useState(false);
 
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const threshold = 200;
+
+            if (scrollY > threshold) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
   return (
-    <div className='flex justify-between w-full h-24 items-center text-white fixed bg-transparent px-5'>
+    <div className={`flex justify-between w-full h-24 items-center text-white fixed px-5 transition-all duration-300 ${isScrolled ? ' bg-slate-950 bg-opacity-95' : 'bg-transparent'}`} style={{ zIndex: 2 }}>
         <img src={logo} alt='logo' className=' h-14 w-15 hover:scale-110 duration-200'/>
         <ul className='hidden md:flex'>
             {links.map(({id, link})=>(<li key={id} className='capitalize  px-7 font-medium text-xl cursor-pointer text-gray-500 hover:scale-125 hover:text-white duration-200'>
@@ -38,7 +60,7 @@ const NavBar = () => {
         <div onClick = {() => setNav(!nav)} className='md:hidden cursor-pointer pr-5 text-gray-500 hover:text-white hover:scale-110 duration-200 z-10'>
             {nav ? <FaTimes size={30}/> : <FaBars size={30}/>}
         </div>
-        {nav && (<ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-slate-600'>
+        {nav && (<ul className='md:hidden flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-slate-600'>
             {links.map(({id, link})=>(<li key={id} className='capitalize px-4 py-6 text-4xl cursor-pointer text-gray-500 hover:scale-125 hover:text-white duration-200'>
             {link}</li>))}
         </ul>)}
